5e0d62ff9e0c54b253b28fecbdf62ef42d963327
Pro. 'Ake Worapot Bhilabutra <44732138+ProAke@users.noreply.github.com>
2021-11-04 20:22:57 +0700

    Remark

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
-
+#####################################################
+# Stockway Application any stock
+# Support understand with barcode and QRCODE
+# Support Linebot, Message API, Tracking, and Others
+####################################################
 
 echo "Welcome to StocyWay Version 0.0.1";
 
